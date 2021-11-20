e555d471bd87df49917fc2b2682bc00ac22c885b
nakkajagadeesh12 <nakkajagadeesh1234@gmail.com>
2021-11-20 21:51:30 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import { QueryClientProvider, QueryClient } from 'react-query';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <QueryClientProvider>
+      <div>
+        <h1>Hello StackBlitz!</h1>
+      </div>
+    </QueryClientProvider>
   );
 }
