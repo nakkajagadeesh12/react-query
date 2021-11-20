561afd37695e147bdf65f1c900f176fd3a1a3f9a
nakkajagadeesh12 <nakkajagadeesh1234@gmail.com>
2021-11-20 21:58:44 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './style.css';
 import { QueryClientProvider, QueryClient } from 'react-query';
 
+const client = new QueryClient();
+
 export default function App() {
   return (
-    <QueryClientProvider>
+    <QueryClientProvider client={client}>
       <div>
         <h1>Hello StackBlitz!</h1>
       </div>
